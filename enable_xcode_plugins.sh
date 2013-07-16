669ebb64a8249b337650aaf130b6fd53947fdf29
Chris Dzombak <chris@chrisdzombak.net>
2013-07-16 16:00:23 -0400

    initial commit of working scripts

---
+++ enable_xcode_plugins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Quit if Xcode is running:
+ps cx | grep -i xcode > /dev/null && echo "Xcode is running; please quit it." && exit
+
+# Use a default storage directory if none is set:
+if [ -z "$XCODE_PLUGIN_STORAGE_DIR" ]; then
+    XCODE_PLUGIN_STORAGE_DIR=$HOME/xcode_plugins
+    echo INFO: Using default plugin storage directory: $XCODE_PLUGIN_STORAGE_DIR
+fi
+
+XCODE_ACTIVE_PLUGIN_DIR=$HOME/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins/
+
+cp -R -p "$XCODE_PLUGIN_STORAGE_DIR"* "$XCODE_ACTIVE_PLUGIN_DIR" && echo "Success."
